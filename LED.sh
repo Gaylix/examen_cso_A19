e22325273c0eefd82d4b8403c58633805cb5e051
Gaylix <gosselinni0@hotmail.com>
2019-07-16 17:54:54 +0000

    modification du code

--- LED.sh
+++ LED.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 # Initialisation des GPIOs. Les GPIOs nécessaires sont activés avec la commande export, puis la direction est choisie.
-echo "71" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio71/direction
+if [ $(cat /sys/class/gpio/gpio71/direction -eq "out" ];
+then
+	echo ""
+else
+	echo "71" > /sys/class/gpio/export
+	echo "out" > /sys/class/gpio/gpio71/direction
+fi
 
-echo "233" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio233/direction
-
-while : 
+if [ $(cat /sys/class/gpio/gpio233/direction -eq "in" ];
+then 
+	echo ""
+else
+	echo "233" > /sys/class/gpio/export
+	echo "in" > /sys/class/gpio/gpio233/direction
+fi
+while true;
 do
 if [ $(cat /sys/class/gpio/gpio233/value) -eq 0 ];
-	then 
+then 
+	if [ $(cat /sys/class/gpio/gpio71/value) -eq 0 ];
+	then
 		echo "1" > /sys/class/gpio/gpio71/value
 	else
 		echo "0" > /sys/class/gpio/gpio71/value
+	fi
 fi
 done
